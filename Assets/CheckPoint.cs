ded98ef756c855e8f20597e66e6ea32098cbd7f7
MYKGamer <mayankpokhriyal098@gmail.com>
2025-03-18 12:42:16 +0530

    update 2.2
    update 2.2

--- Assets/CheckPoint.cs
+++ Assets/CheckPoint.cs
@@ -3,16 +3,22 @@ using UnityEngine;
 public class CheckPoint : MonoBehaviour
 {
    GameManager gameManager;
+   public Transform RespawnPoint;
+   SpriteRenderer spriteRenderer;
+   public Sprite passive, active;
 
    void Awake()
    {
         gameManager = GameObject.FindGameObjectWithTag("Player").GetComponent<GameManager>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        
    }
    void OnTriggerEnter2D(Collider2D collision )
    {
         if(collision.CompareTag("Player"))
         {
-            gameManager.UpdateCheckPoint(transform.position);
+            gameManager.UpdateCheckPoint(RespawnPoint.position);
+            spriteRenderer.sprite = active;
         }
    }
 }
